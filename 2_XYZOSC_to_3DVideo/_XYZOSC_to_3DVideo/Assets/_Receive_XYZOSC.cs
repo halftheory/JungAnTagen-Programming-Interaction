3e88b65a01975c9006dc1a0fdbdc836aadee8287
Half/theory <noreply@halftheory.com>
2018-07-24 16:38:45 +0200

    update

---
+++ 2_XYZOSC_to_3DVideo/_XYZOSC_to_3DVideo/Assets/_Receive_XYZOSC.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _Receive_XYZOSC : MonoBehaviour {
+    
+   	public OSC osc;
+    public GameObject prefab;
+    public GameObject groupParent;
+
+	// Use this for initialization
+	void Start () {
+        osc.SetAllMessageHandler(messageHandler);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void messageHandler(OscMessage message){
+        //Debug.Log(message.address);
+        makeInstance(message);
+    }
+
+    void makeInstance(OscMessage message) {
+        float x = message.GetFloat(0);
+        float y = message.GetFloat(1);
+        float z = message.GetFloat(2);
+        // flip z
+        z = 1 - z;
+        GameObject gameObject = Instantiate(prefab, new Vector3(x,y,z), Quaternion.identity, groupParent.transform);
+        Destroy(gameObject,0.1f);
+    }
+
+}
